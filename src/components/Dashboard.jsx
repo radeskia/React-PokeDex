d27601201d8d2a49a27aa3fe72363cfd73b29fec
radeskia <radeskia331@gmail.com>
2022-03-25 01:26:21 +0100

    Added Pagination

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,36 +2,74 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DashboardCards from "./DashboardCards";
 import { Container, Row } from "react-bootstrap";
+import ReactPaginate from "react-paginate";
+import "./../assets/pagination.css";
 
 const Dashboard = () => {
-  const [data, setData] = useState();
+    const [data, setData] = useState();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get("https://pokeapi.co/api/v2/pokemon/");
-      setData(res.data);
+    const [pageNumber, setPageNumber] = useState(0);
+    const pokemonPerPage = 24;
+    const pastPokemon = pageNumber * pokemonPerPage;
+
+    const changePage = ({ selected }) => {
+        setPageNumber(selected);
     };
-    fetchData();
-  }, []);
 
-  return (
-    <div className="bg-dark">
-      <Container className="d-flex flex-wrap justify-content-center">
-        <Row
-          xs={2}
-          md={3}
-          lg={4}
-          xl={12}
-          className="justify-content-center py-5"
-        >
-          {data &&
-            data.results.map((x, index) => {
-              return <DashboardCards data={x} key={index} />;
-            })}
-        </Row>
-      </Container>
-    </div>
-  );
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await axios.get(
+                "https://pokeapi.co/api/v2/pokemon?offset=0&limit=151"
+            );
+            setData(res.data);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div className="bg-dark">
+            <Container className="d-flex flex-wrap justify-content-center">
+                <Row
+                    xs={2}
+                    md={3}
+                    lg={4}
+                    xl={12}
+                    className="justify-content-center py-5"
+                >
+                    {data
+                        ? data.results
+                              .slice(pastPokemon, pastPokemon + pokemonPerPage)
+                              .map((x, index) => {
+                                  return (
+                                      <DashboardCards
+                                          data={x}
+                                          key={index}
+                                          pageNumber={pageNumber}
+                                      />
+                                  );
+                              })
+                        : null}
+
+                    {data && (
+                        <ReactPaginate
+                            previousLabel={"<"}
+                            nextLabel={">"}
+                            pageCount={Math.ceil(
+                                data.results.length / pokemonPerPage
+                            )}
+                            onPageChange={changePage}
+                            containerClassName={"paginationButtons"}
+                            previousLinkClassName={"previousButton"}
+                            nextLinkClassName={"nextButton"}
+                            activeClassName={"paginationActive"}
+                        />
+                    )}
+                </Row>
+            </Container>
+        </div>
+    );
 };
+/*
 
+*/
 export default Dashboard;
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -3,23 +3,28 @@ import { Container, Row, Col } from "react-bootstrap";
 import logo from "../img/img1.png";
 
 const Showcase = () => {
-  return (
-    <div className="bg-light text-dark text-center p-5">
-      <Container>
-
-        <div>
-          <Row className="my-5" xs={1} sm={2}>
-            <Col className="p-3">
-              <img src={logo} className="img-fluid" width="400rem" alt="" />{" "}
-            </Col>
-            <Col className="p-3 align-self-center">
-                PokeDex project made in React using react-bootstrap and the pokemon API.
-            </Col>
-          </Row>
+    return (
+        <div className="bg-light text-dark text-center p-5">
+            <Container>
+                <div>
+                    <Row className="my-5" xs={1} sm={2}>
+                        <Col className="p-3">
+                            <img
+                                src={logo}
+                                className="img-fluid"
+                                width="400rem"
+                                alt=""
+                            />{" "}
+                        </Col>
+                        <Col className="p-3 align-self-center">
+                            PokeDex project made in React using react-bootstrap
+                            and the pokemon API.
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
         </div>
-      </Container>
-    </div>
-  );
+    );
 };
 
 export default Showcase;
